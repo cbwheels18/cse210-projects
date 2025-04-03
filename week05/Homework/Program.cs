9fde6ef567b8ddf786415a82199d2f48a8ad82d1
cbwheels18@gmail.com <Cody@settersuite.com>
2025-04-03 15:36:41 -0600

    created the homework assigment, with Assingment, MathAssignment, and WritingAssignment and then called those classes

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Matthew Jonhson", "math");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("John Doey", "Math", "Section 5.2", "Problems 1-10");
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Jackson Dew", "History", "The Great War");
+        Console.WriteLine(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
